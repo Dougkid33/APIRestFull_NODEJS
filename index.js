7b3739719da44c1257c007fde76722b5f01a5d91
Dougkid33 <douglasaugustomsilva@hotmail.com>
2023-06-23 15:02:30 -0300

    secretMoongoseurl

--- index.js
+++ index.js
@@ -27,11 +27,3 @@ app.get('/', (req, res) =>{
 
 
 
-mongoose
-    .connect("mongodb+srv://douglasaugustomsilva:Shippuden33@cluster0.y5uebon.mongodb.net/?retryWrites=true&w=majority"
-    )
-    .then(()=>{
-        console.log("Conectado ao Mongo")
-        app.listen(3000)
-    })//tratamento ou seja then quando deu certo a conexao
-    .catch((err) => console.log(err))//aqui quando ha um erro
